0389c8978cb7f674e0afbc6006b5e9ab228d0918
Shintaro Kurachi <shintaro_kurachi@cybird.co.jp>
2012-03-12 20:57:50 +0900

    mocha対応の忘れ

---
+++ test/calc.test.js
@@ -0,0 +1,13 @@
+var should = require('should');
+
+describe('calc', function() {
+  describe('.add', function() {
+    it('should return sum of 2 arguments', function() {
+      Number(3).should.equal(3);
+    });
+
+    it('error', function() {
+      Number(3).should.equal(3);
+    });
+  });
+});
